bf15912bdc450d2c5e3255ad29d51f5fbde2e776
moritzjuergens <moritzjuergens@outlook.de>
2022-07-09 13:13:42 +0200

    a

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 import os
+from uuid import uuid4
 from flask import Flask, Response, request
 import requests
 import json
@@ -41,6 +42,65 @@ def questions():
         return Response(status=500)
 
 
+@app.route("/start/<name>/<question_cnt>", methods=["GET"])
+def start(name, question_cnt):
+
+    try:
+        res = client.postgrest.from_("questions").select("*").execute()
+    except Exception as err:
+        print(err)
+
+    questions = []
+    print(questions)
+    #questions['answer_given'] = 0
+    for question in res.data:
+        temp = {
+            "id": question['id'],
+            "question": question['question'],
+            "answers": question["answers"],
+            "corr_idx": question["corr_idx"],
+            "answer_given": 0,
+        }
+        questions.append(temp)
+    print(questions)
+    game_id = str(uuid4())
+    game_info = json.dumps({
+        "name": name,
+        "questions": questions
+    })
+
+    try:
+        client.postgrest.from_("games").insert({
+            "game_id": game_id,
+            "game_info": game_info
+        }).execute()
+    except Exception as err:
+        print(err)
+        return Response(status=600)
+
+    return Response(json.dumps({"game_id": game_id, "game_info": game_info}), status=200, mimetype='application/json')
+
+
+@app.route("/answer", methods=["POST"])
+def check_answer():
+    payload = request.json
+
+    try:
+        game = client.postgrest.from_table("games").select(
+            "*").query("WHERE game_id=" + payload.game_id).execute()
+    except Exception as err:
+        print(err)
+        return Response(status=500)
+
+    for question in game.questions:
+        if(question.id != payload.question_id):
+            continue
+        question.answer_given = payload.answer
+
+        return Response(json.dumps({answer: "correct"}))
+    return Response(json.dumps({answer: "incorrect"}))
+
+
 @app.route("/results", methods=["POST"])
 def store_results():
     payload = request.json
@@ -56,8 +116,8 @@ def store_results():
         return Response(status=500)
 
 
-@app.route("/answers", methods=["POST"])
-def store_results():
+@app.route("/questions", methods=["POST"])
+def post_questions():
     payload = request.json
 
     try:
@@ -73,5 +133,5 @@ def store_results():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 7777))
     app.run(host='0.0.0.0', port=port)
